f01c9e080067bf1e7c343d7dbe7382275d73756d
Eduardo Lerma <42332011+LaloCode@users.noreply.github.com>
2023-10-03 02:33:52 +0000

    Move Course module to its own file

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,37 +1,5 @@
 /* eslint-disable react/prop-types */
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
-
-const Total = (props) => {
-  return <p>Total of {props.sumOfExercises} exercises</p>
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({parts}) => {
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} />)}
-    </div>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sumOfExercises={course.parts.reduce((accumulator, currrentValue) => accumulator + currrentValue.exercises, 0)} />
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [

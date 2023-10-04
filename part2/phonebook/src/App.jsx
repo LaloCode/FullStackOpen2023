9fe59fc639a8898819b57d5be9d970d8d8c13c67
Eduardo Lerma <42332011+LaloCode@users.noreply.github.com>
2023-10-04 23:36:08 +0000

    Separate  components  reset number after change

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,6 +5,30 @@ const Number = ({name, number}) => {
   return <p>{name} {number}</p>
 }
 
+const Filter = ({value, handleFilterChange}) => {
+  return (
+    <div>
+      Filter shown with: <input value={value} onChange={handleFilterChange}/>
+    </div>
+  )
+}
+
+const PersonForm = ({addPerson, newName, newNumber, handleNameChange, handleNumberChange}) => {
+  return (
+    <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+    </form>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
@@ -33,26 +57,15 @@ const App = () => {
     }
     setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
   } 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          Filter shown with: <input value={filter} onChange={handleFilterChange}/>
-        </div>
+      <Filter value={filter} handleFilterChange={handleFilterChange}/>
       <h2>Add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
       <h2>Numbers</h2>
       {persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? <Number key={person.name} name={person.name} number={person.number}/> : null)}
     </div>

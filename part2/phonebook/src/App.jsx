56cf2250fd86730e62a9fee5d119e569a62bdb30
LaloCode <lalo_pichu22@hotmail.com>
2023-10-03 19:11:36 -0600

    Add new number field

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
-const Number = ({name}) => {
-  return <p>{name}</p>
+const Number = ({name, number}) => {
+  return <p>{name} {number}</p>
 }
 
 const App = () => {
@@ -9,9 +10,14 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -20,7 +26,7 @@ const App = () => {
       alert(`${newName} is already added to the phonebook`)
       return
     }
-    setPersons(persons.concat({name: newName}))
+    setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
   } 
 
@@ -32,11 +38,14 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <Number  key={person.name} name={person.name}/>)}
+      {persons.map((person) => <Number  key={person.name} name={person.name} number={person.number}/>)}
     </div>
   )
 }

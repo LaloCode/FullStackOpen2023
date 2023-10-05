171d6cda90a9b71ec928523b11ae5d1b6398aa8b
Eduardo Lerma <42332011+LaloCode@users.noreply.github.com>
2023-10-05 01:08:43 +0000

    use json-server for axios

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,75 +1,100 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Number = ({name, number}) => {
-  return <p>{name} {number}</p>
-}
+const Number = ({ name, number }) => {
+  return (
+    <p>
+      {name} {number}
+    </p>
+  );
+};
 
-const Filter = ({value, handleFilterChange}) => {
+const Filter = ({ value, handleFilterChange }) => {
   return (
     <div>
-      Filter shown with: <input value={value} onChange={handleFilterChange}/>
+      Filter shown with: <input value={value} onChange={handleFilterChange} />
     </div>
-  )
-}
+  );
+};
 
-const PersonForm = ({addPerson, newName, newNumber, handleNameChange, handleNumberChange}) => {
+const PersonForm = ({
+  addPerson,
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+}) => {
   return (
     <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
     </form>
-  )
-}
+  );
+};
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
 
   const handleFilterChange = (event) => {
-    setFilter(event.target.value)
-  }
+    setFilter(event.target.value);
+  };
 
   const addPerson = (event) => {
-    event.preventDefault()
-    if (persons.some((person => person.name === newName))) {
-      alert(`${newName} is already added to the phonebook`)
-      return
+    event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to the phonebook`);
+      return;
     }
-    setPersons(persons.concat({name: newName, number: newNumber}))
-    setNewName('')
-    setNewNumber('')
-  } 
+    setPersons(persons.concat({ name: newName, number: newNumber }));
+    setNewName('');
+    setNewNumber('');
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter value={filter} handleFilterChange={handleFilterChange}/>
+      <Filter value={filter} handleFilterChange={handleFilterChange} />
       <h2>Add a new</h2>
-      <PersonForm addPerson={addPerson} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+      />
       <h2>Numbers</h2>
-      {persons.map((person) => person.name.toLowerCase().includes(filter.toLowerCase()) ? <Number key={person.name} name={person.name} number={person.number}/> : null)}
+      {persons.map((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase()) ? (
+          <Number key={person.name} name={person.name} number={person.number} />
+        ) : null
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
